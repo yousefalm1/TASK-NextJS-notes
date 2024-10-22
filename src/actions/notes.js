35101a2f8fd6c35f34cd63610b44b87d032812fe
Octowl <alsaff1987@gmail.com>
2024-10-22 17:37:34 +0300

    fetch note by id

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,7 +1,7 @@
 'use server'
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 const baseUrl = "https://task-react-auth-backend.eapi.joincoded.com/api";
 const headers = new Headers()
@@ -11,6 +11,15 @@ export async function fetchNotes() {
   const response = await fetch(`${baseUrl}/notes`);
   const notes = await response.json()
   return notes
+}
+
+export async function fetchNoteById(noteId) {
+  const response = await fetch(`${baseUrl}/notes/${noteId}`);
+
+  if (!response.ok) notFound()
+
+  const note = await response.json()
+  return note
 }
 
 export async function createNote(formData) {
