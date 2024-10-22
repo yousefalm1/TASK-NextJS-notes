788a3a1b31062c6884e716aa13890a5c74026ce7
Octowl <alsaff1987@gmail.com>
2024-10-22 18:20:49 +0300

    getAllUsers

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,17 @@
 'use server'
 
+import { baseUrl } from "./config"
+
 export async function login(formData) {
   console.log(formData)
 }
 
 export async function register(formData) {
   console.log(formData)
+}
+
+export async function getAllUsers() {
+  const response = await fetch(`${baseUrl}/auth/users`)
+  const users = response.json()
+  return users
 }
