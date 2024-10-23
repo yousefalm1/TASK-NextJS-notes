bb4c9f229679e9b8980f1b3c7b52f09f84a7877c
Octowl <alsaff1987@gmail.com>
2024-10-23 14:43:23 +0300

    remove clg

--- src/components/UserList.js/index.js
+++ src/components/UserList.js/index.js
@@ -1,7 +1,6 @@
 import UserCard from "./UserCard"
 
 function UserList({ users }) {
-  console.log(users)
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
       {users.map((user) => <UserCard key={user._id} user={user} />)}
