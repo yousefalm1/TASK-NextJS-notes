4ac0705d570c9195d13866a0bbc505057f7e4b60
Octowl <alsaff1987@gmail.com>
2024-10-23 14:21:45 +0300

    conditional add button

--- src/app/notes/page.js
+++ src/app/notes/page.js
@@ -1,13 +1,15 @@
 import { fetchNotes } from "@/actions/notes";
 import AddNoteModal from "@/components/AddNoteModal";
 import NoteList from "@/components/NoteList";
+import { getUser } from "@/lib/token";
 
 async function Notes() {
   const notes = await fetchNotes()
+  const user = await getUser()
 
   return (
     <div className="p-5 min-h-screen bg-gray-900">
-      <AddNoteModal />
+      {user && <AddNoteModal />}
       <NoteList notes={notes} />
     </div>
   );
